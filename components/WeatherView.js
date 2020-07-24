57c30ad36e18be8ad51bf32a889a19c3074dde07
josh <joshua_satriano@homedepot.com>
2020-07-24 11:06:54 -0500

    add basic temp and feel like Texts

--- components/WeatherView.js
+++ components/WeatherView.js
@@ -5,7 +5,10 @@ import { OPEN_WEATHER_API_KEY } from '@env'
 export default class WeatherView extends React.Component {
   state = {
     time: '',
-    location: ''
+    location: '',
+    icon: '',
+    temperature: null,
+    feelsLike: null
   }
 
   componentDidMount() {
@@ -31,7 +34,12 @@ export default class WeatherView extends React.Component {
         let date = new Date(newtime * 1000);
         let name = data.name + ', ' + data.sys.country
 
-        this.setState({location: name});
+        this.setState({
+          location: name,
+          icon: data.weather[0].icon,
+          temperature: data.main.temp,
+          feelsLike: data.main.feels_like
+        });
         this.props.updateImage(isDay);
   		});
     }
@@ -41,6 +49,14 @@ export default class WeatherView extends React.Component {
     return (
       <View style={styles.weatherViewStyle}>
         <Text style={styles.locationStyle}>{this.state.location}</Text>
+        <Image
+          source={imageMap[this.state.icon]}
+        />
+        <Text style={styles.locationStyle}>Temperature</Text>
+        <Text>{this.state.temperature}</Text>
+        <Text style={styles.locationStyle}>Feels Like</Text>
+        <Text>{this.state.feelsLike}</Text>
+
       </View>
     );
   }
@@ -53,10 +69,33 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 20,
     opacity: 0.95,
-    marginTop: '10%'
+    marginTop: '10%',
+    alignItems: 'center',
   },
   locationStyle: {
     fontSize: 30,
-    fontFamily: 'Arial, Helvetica, sans-serif',
+    marginTop: '5%',
+    fontFamily: 'Arial Rounded MT Bold',
   }
 });
+
+const imageMap = {
+  '01d': require('../assets/icons/01d.png'),
+  '01n': require('../assets/icons/01n.png'),
+  '02d': require('../assets/icons/02d.png'),
+  '02n': require('../assets/icons/02n.png'),
+  '03d': require('../assets/icons/03d.png'),
+  '03n': require('../assets/icons/03n.png'),
+  '04d': require('../assets/icons/04d.png'),
+  '04n': require('../assets/icons/04n.png'),
+  '09d': require('../assets/icons/09d.png'),
+  '09n': require('../assets/icons/09n.png'),
+  '10d': require('../assets/icons/10d.png'),
+  '10n': require('../assets/icons/10n.png'),
+  '11d': require('../assets/icons/11d.png'),
+  '11n': require('../assets/icons/11n.png'),
+  '13d': require('../assets/icons/13d.png'),
+  '13n': require('../assets/icons/13n.png'),
+  '50d': require('../assets/icons/50d.png'),
+  '50n': require('../assets/icons/10n.png'),
+}
